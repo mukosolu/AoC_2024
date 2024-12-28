d878836242edbd7f1144c57e6285120b561f9fba
Primrose <mukosolu@users.noreply.github.com>
2024-12-28 07:41:33 +0000

    test function stubs

---
+++ 2/day2_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPart2Addition(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
+
+func TestAmIaTrueLevel(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
+
+func TestMain(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
---
+++ myFunctions/myFunctions_test.go
@@ -0,0 +1,54 @@
+package myFunctions
+
+import (
+	"testing"
+)
+
+func TestGetLineScannerFromFile(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
+
+func TestGetIntFromWordScanner(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
+
+func TestGetWordScannerFromFile(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
+
+func TestGetWordScannerFromLine(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
+
+func TestFuncTimer(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
+
+func TestCheck(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
+
+func TestAbs(t *testing.T) {
+	//create input
+	//describe expected output
+	//run function
+	//check output
+}
